2d97b42239f11cfbc1735c27ae14e953cca9302c
Napat <napat.wongchr@gmail.com>
2020-10-20 22:29:53 +0700

    add ex9 answer

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -4,15 +4,18 @@ import { UserContext } from "../../contexts/user-context";
 import "./index.css";
 
 function PostList(props) {
+  const { posts, ...rest } = props;
   return (
     <div className="post-list">
       {props.posts.map((post) => {
         return (
           <Post
             key={post.id}
+            id={post.id}
             title={post.title}
             description={post.description}
             author={post.author}
+            {...rest}
           />
         );
       })}
@@ -21,8 +24,16 @@ function PostList(props) {
 }
 
 function Post(props) {
-  const { title, description, author } = props;
+  const {
+    id,
+    title,
+    description,
+    author,
+    handleEditPost,
+    handleDeletePost,
+  } = props;
   const { theme } = useContext(ThemeContext);
+
   const postClass = theme === "light" ? "post" : "post post-dark";
   const postTitlteClass =
     theme === "light" ? "post-title" : "post-title post-title-dark";
@@ -32,19 +43,27 @@ function Post(props) {
       : "post-description post-description-dark";
   const postAuthor =
     theme === "light" ? "post-author" : "post-author post-author-dark";
+
   return (
     <div className={postClass}>
       <h1 className={postTitlteClass}>{title}</h1>
       <p className={postDescription}>{description}</p>
       <p className={postAuthor}>Author: {author}</p>
-      <ActionMenu />
+      <ActionMenu
+        postId={id}
+        handleEditPost={handleEditPost}
+        handleDeletePost={handleDeletePost}
+      />
     </div>
   );
 }
 
-function ActionMenu() {
+function ActionMenu(props) {
+  const { postId, handleEditPost, handleDeletePost } = props;
+
   const user = useContext(UserContext);
   const { theme } = useContext(ThemeContext);
+
   const postUserClass =
     theme === "light" ? "post-user" : "post-user post-user-dark";
   const editButtonClass =
@@ -55,11 +74,22 @@ function ActionMenu() {
     theme === "light"
       ? "button post-delete-action-button"
       : "button post-delete-action-button post-delete-action-button-dark";
+
   return (
     <div className="post-actions">
       <span className={postUserClass}>You are {user.user}</span>
-      <button className={editButtonClass}>Edit</button>
-      <button className={deleteButtonClass}>Delete</button>
+      <button
+        className={editButtonClass}
+        onClick={() => handleEditPost(postId)}
+      >
+        Edit
+      </button>
+      <button
+        className={deleteButtonClass}
+        onClick={() => handleDeletePost(postId)}
+      >
+        Delete
+      </button>
     </div>
   );
 }
--- src/pages/Exercise9/index.js
+++ src/pages/Exercise9/index.js
@@ -4,10 +4,46 @@ import { UserContext } from "../../contexts/user-context";
 import Instruction from "../../components/Instruction";
 import PostList from "../../components/PostList";
 import mockedPosts from "../../mocks/posts";
+import "./index.css";
 
 function Exercise9() {
   const [user] = useState("John");
   const [theme] = useState("light");
+  const [posts, setPosts] = useState(mockedPosts);
+
+  function handleDeletePost(id) {
+    const newPosts = posts.filter((post) => post.id !== id);
+    setPosts(newPosts);
+  }
+
+  function handleEditPost(id) {
+    // copy new array and manipulate them,
+    // we should not manipulate posts state directly
+    // You can use lodash or simple Array.map trick to clone deeply
+    const newPosts = posts.map((post) => {
+      return { ...post };
+    });
+    const postIndex = newPosts.findIndex((post) => post.id === id);
+    const newPostDescription = prompt("Edit new post description");
+    newPosts[postIndex].description = newPostDescription;
+    setPosts(newPosts);
+  }
+
+  function handleAddPost() {
+    const newTitle = prompt("Add post title");
+    const newDescription = prompt("Add post description");
+
+    const newPost = {
+      id: "_" + Math.random().toString(36).substr(2, 9),
+      title: newTitle,
+      description: newDescription,
+      author: "John เอง",
+    };
+    // We should not do posts.push(),
+    // best practice is to avoid manipulate mutate states directly
+    // so, we create new array and add newPost at the end of array
+    setPosts([...posts, newPost]);
+  }
 
   return (
     <div className="page-container">
@@ -15,9 +51,16 @@ function Exercise9() {
         topic="Posts App (useState)"
         description="Make Post App be able to Add / Edit / Delete with useState"
       />
+      <div className="page-add-post-button" onClick={handleAddPost}>
+        Add
+      </div>
       <ThemeContext.Provider value={{ theme }}>
         <UserContext.Provider value={{ user: user }}>
-          <PostList posts={mockedPosts} />
+          <PostList
+            posts={posts}
+            handleEditPost={handleEditPost}
+            handleDeletePost={handleDeletePost}
+          />
         </UserContext.Provider>
       </ThemeContext.Provider>
     </div>
